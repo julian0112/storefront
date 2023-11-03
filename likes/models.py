7541a33de80e04e3ee2d501a3836efa6335c3cf2
Julian <julian.lsanchez01@gmail.com>
2023-11-03 14:36:27 -0600

    Added relationships in store/models, tags/models. Created likes app and added models with relationships

--- likes/models.py
+++ likes/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.auth.models import User
 
-
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-    
-class TaggedItem(models.Model):
-    #What tag is applied to what object
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+class LikedItems(models.Model):
     #Generic relationship structure
     contentType = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
+    
+    #Generic user model
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- store/models.py
+++ store/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()  
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        'Product', on_delete= models.SET_NULL, null=True, related_name='+')
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
     
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)
@@ -19,9 +30,9 @@ class Customer(models.Model):
     MEMBERSHIP_GOLD = 'G'
     
     MEMBERSHIP_CHOICES = [
-        ('MEMBERSHIP_BRONZE', 'Bronze'),
-        ('MEMBERSHIP_SILVER', 'Silver'),
-        ('MEMBERSHIP_GOLD', 'Gold')
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold')
     ]
     
     membership = models.CharField(
@@ -41,4 +52,23 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PENDING_PAYMENT_STATUS)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
