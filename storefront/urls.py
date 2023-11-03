720b4387c22c7b1fc9a84559b5aff754f5a8cfa6
Julian <julian.lsanchez01@gmail.com>
2023-11-03 13:11:20 -0600

    Created store and tags apps

--- storefront/urls.py
+++ storefront/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls"))
 ]
