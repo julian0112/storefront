07aa9f3c382b52f1aabace586cd1655d5408feb2
Julian <julian.lsanchez01@gmail.com>
2023-11-03 22:55:34 -0600

    Basic ORM in playground app

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from store.models import Product
 
-# Create your views here.
+def say_hello(request):
+    query_set = Product.objects.all()
+    query_set.filter().filter().order_by()
+    
+        
+    return render(request, 'hello.html', {'name': 'Julian'})
--- storefront/urls.py
+++ storefront/urls.py
@@ -20,5 +20,6 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls"))
+    path("__debug__/", include("debug_toolbar.urls")),
+    path('playground/', include('playground.urls'))
 ]
