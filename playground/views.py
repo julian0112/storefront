65036e3c4821abf24e97341512d3b50b73bb0787
Julian <julian.lsanchez01@gmail.com>
2023-11-05 21:11:26 -0600

    Test queary sorting in the playground app

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from store.models import Product
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q, F
+from store.models import OrderItem, Product, Order
 
 def say_hello(request):
-    query_set = Product.objects.all()
-    query_set.filter().filter().order_by()
-    
+    # quearyset = Product.objects.prefetch_related('promotions').select_related('collection').all()
+    quearyset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
         
-    return render(request, 'hello.html', {'name': 'Julian'})
+    return render(request, 'hello.html', {'name': 'Julian', 'orders': list(quearyset)})
